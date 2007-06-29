29dafd294ad01781a8aa09c283634ee6ec6cdd43
mfriese <mfriese@f537dc7c-6d33-0410-91ad-f2f893324d72>
2007-06-29 15:24:53 +0000

    Initial commit of testcode

---
+++ Sandbox/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "TestLib.h"
+
+int main (int argc, char * const argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+	
+	TestLib t;
+	t.println();
+	
+    return 0;
+}
