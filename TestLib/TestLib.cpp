839f7f7232f2cb1b104d23a896dc2eadefa06210
mfriese <mfriese@f537dc7c-6d33-0410-91ad-f2f893324d72>
2007-06-29 15:31:41 +0000

    Initial commit of testcode

---
+++ TestLib/TestLib.cpp
@@ -0,0 +1,15 @@
+/*
+ *  TestLib.cpp
+ *  TestLib
+ *
+ *  Created by Markus Friese on 28.06.07.
+ *  Copyright 2007 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "TestLib.h"
+
+
+void TestLib::println() {
+	std::cout << "--- lib output ---\n";
+}
---
+++ TestLib/TestLib.h
@@ -0,0 +1,17 @@
+/*
+ *  TestLib.h
+ *  TestLib
+ *
+ *  Created by Markus Friese on 28.06.07.
+ *  Copyright 2007 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include <iostream>
+
+class TestLib {
+
+public:
+	void println(void);
+
+};
